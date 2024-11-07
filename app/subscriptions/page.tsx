f02ad04a491e90c479553fb91180f6430a0a4692
Roberta <roberta.amarto@ed3.com.br>
2024-11-06 21:56:19 -0300

    feat: create schema.prisma

--- app/subscriptions/page.tsx
+++ app/subscriptions/page.tsx
@@ -1,7 +1,7 @@
 const Subscription = () => {
-  return(
-  <h1>Subscription page</h1>
+  return (
+    <h1>Subscription page</h1>
   )
-  }
-  
-  export default Subscription;
+}
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,8 @@
+
+const Transactions = () => {
+  return (
+    <h1>Transactions Page</h1>
+  )
+}
+
+export default Transactions
